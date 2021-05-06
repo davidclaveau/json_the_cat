ace5703b5de32481c74a546a69e4a60b61d65f88
David Claveau <daveclaveau@gmail.com>
2021-05-06 19:59:41 +0000

    Made some changes to how the breedFetcher handles errors or missing information.

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,11 +4,16 @@ const arg = process.argv.slice(2);
 const website = `https://api.thecatapi.com/v1/breeds/search?q=`;
 const search = website + arg;
 
-request(search, (error, response, body) => {
-  console.error('error:', error); // Print the error if one occurred
+request(search, (err, response, body) => {
+  if (err) {
+    console.log("Error in request.");
+    console.log(err);
+    return;
+  }
+
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   const data = JSON.parse(body);
-  console.log("data[0]", data[0]);
+
 
   if (!data[0]) {
     console.log("Data not found. Check spelling!");
@@ -16,5 +21,4 @@ request(search, (error, response, body) => {
   }
 
   console.log(data[0].description);
-
 });
